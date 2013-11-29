1fc90168b9e09126ab0bb59b1224254023282c7a
José Di Meglio <jgdimeglio@gmail.com>
2013-11-29 18:07:36 +0100

    Update resumenes.rb
    Te gusta ahi!

--- app/controllers/resumenes.rb
+++ app/controllers/resumenes.rb
@@ -14,7 +14,17 @@ Resumenes::App.controllers :resumenes do
 
   get :nuevo, :map => '/crear_resumen' do
     @resumen = Resumen.new
-    @proximoAlumno = Alumno.proximo_alumno()
+    @alumnosOrdenados = Alumno.all(:order => [:cant_resumenes.asc, :apellido.asc, :nombre.asc])
+     # @alumnosOrdenados.each do | i |
+     #   puts '-------------------------'
+     #   puts 'APELLIDO:'
+     #   puts i.apellido
+     #   puts 'NOMBRE'
+     #   puts i.nombre
+     #   puts 'CANTIDAD'
+     #   puts i.cant_resumenes
+     # end
+     @proximoAlumno = @alumnosOrdenados.first
     render 'resumenes/nuevo'
   end
 
