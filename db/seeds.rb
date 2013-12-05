9293520587218f6a235886b94eadd2a4ad529f19
jgdimeglio <jgdimeglio@gmail.com>
2013-12-04 23:06:07 -0300

    Se arreglarón los nonmbre de los alumnos en el seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,42 @@
 alumno = Alumno.create(
 			:email => 'pgassman@gmail.com',
 		   	:nombre => 'aablo',
-			:apellido => 'Aassman', 
+			:apellido => 'Gassman', 
 			:cant_resumenes => 0,
 		   	:contrasena => "Admin0n!")
 
 alumno = Alumno.create(
 			:email => 'lgassman@gmail.com',
 		   	:nombre => 'Lucas',
-			:apellido => 'Bassman',
+			:apellido => 'Gassman',
 			:cant_resumenes => 0, 
 		   	:contrasena => "Admin0n!")
 
 alumno = Alumno.create(
 			:email => 'lrrinaudo@gmail.com',
 		   	:nombre => 'Leandro',
-			:apellido => 'Cinaudo',
+			:apellido => 'Rinaudo',
 			:cant_resumenes => 0, 
 		   	:contrasena => "Admin0n!")
 
 alumno = Alumno.create(
 			:email => 'mrinaudo@gmail.com',
 		   	:nombre => 'Martin',
-			:apellido => 'Dinaudo',
+			:apellido => 'Rinaudo',
 			:cant_resumenes => 0, 
 		   	:contrasena => "Admin0n!")
 
 alumno = Alumno.create(
 			:email => 'sponce@gmail.com',
 		   	:nombre => 'Saira',
-			:apellido => 'Eonce',
+			:apellido => 'Ponce',
 			:cant_resumenes => 0, 
 		   	:contrasena => "Admin0n!")
 
 alumno = Alumno.create(
 			:email => 'jdimeglio@gmail.com',
 		   	:nombre => 'Jose',
-			:apellido => 'Fimeglio',
+			:apellido => 'Dimeglio',
 			:cant_resumenes => 0, 
 		   	:contrasena => "Admin0n!")
 
